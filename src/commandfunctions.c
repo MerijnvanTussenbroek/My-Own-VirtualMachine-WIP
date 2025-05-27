da3258a6e4e503bacb733dda90159318809bbe49
Merijn van Tussenbroek <m.j.w.vantussenbroek@students.uu.nl>
2025-05-27 09:52:26 +0200

    implemented the LOAD function

--- src/commandfunctions.c
+++ src/commandfunctions.c
@@ -88,7 +88,7 @@ void SET_func(VM* vm)
     length = localList->size;
     for(int i = 0; i < length; i++)
     {
-        args_result result2 = args_retrieveFromList(globalList, i);
+        args_result result2 = args_retrieveFromList(localList, i);
         if(result2.success == 1)
         {
             if(strcpy(result2.value.name, varName))
@@ -96,7 +96,8 @@ void SET_func(VM* vm)
                 Args* updatedValue2 = &result2.value;
 
                 updatedValue2->value = v;
-                args_changeInList(globalList, *updatedValue2, i);
+                args_changeInList(localList, *updatedValue2, i);
+                frame_push(vm->frames, &stackItem.value);
                 return;
             }
         }
@@ -105,7 +106,68 @@ void SET_func(VM* vm)
 
 void LOAD_func(VM* vm)
 {
-    
+        values_stack* stack = vm->loadedValues;
+    values_stack_ss value = values_pop(stack);
+
+    if(value.success != 1)
+    {
+        exit(-1);
+    }
+
+    unsigned long int v = value.value;
+
+    command c = vm->program[vm->r.ip];
+    values val = c.argument[0];
+
+    if(val.type != STRING)
+    {
+        exit(-1);
+    }
+
+    char* varName = val.value.name;
+
+    //we first look through global variables
+    // then through local ones
+
+    values_stack* stack = vm->loadedValues;
+
+    args_list* globalList = vm->globalVariables;
+    int length = globalList->size;
+    for(int i = 0; i < length; i++)
+    {
+        args_result result = args_retrieveFromList(globalList, i);
+        if(result.success == 1)
+        {
+            if(strcpy(result.value.name, varName))
+            {
+                values_push(stack, result.value.value);
+                return;
+            }
+        }
+    }
+
+    frame_stack_ss stackItem = frame_pop(vm->frames);
+
+    if(stackItem.success != 1)
+    {
+        exit(-1);
+    }
+
+    args_list* localList = stackItem.value.args;
+    length = localList->size;
+    for(int i = 0; i < length; i++)
+    {
+        args_result result2 = args_retrieveFromList(localList, i);
+        if(result2.success == 1)
+        {
+            if(strcpy(result2.value.name, varName))
+            {
+                values_push(stack, result2.value.value);
+                frame_push(vm->frames, &stackItem.value);
+                return;
+            }
+        }
+    }
 }
 
 void PUSH_func(VM* vm)
